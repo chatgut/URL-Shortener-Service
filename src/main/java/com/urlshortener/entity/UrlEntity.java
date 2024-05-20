4442e17dc2cdd2d500bace8db17fc8b4ebfd4583
Marcus <marcus.olsson98@gmail.com>
2024-05-20 21:04:31 +0200

    Add UrlEntity

---
+++ src/main/java/com/urlshortener/entity/UrlEntity.java
@@ -0,0 +1,58 @@
+package com.urlshortener.entity;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "shortened_urls")
+public class UrlEntity {
+
+    @Id
+    private String id;
+    private String originalUrl;
+    private String shortUrl;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    public UrlEntity(String id, String originalUrl, String shortUrl) {
+        this.id = id;
+        this.originalUrl = originalUrl;
+        this.shortUrl = shortUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
